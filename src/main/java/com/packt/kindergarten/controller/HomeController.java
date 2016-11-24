240264644f0aa668b2edfdb8ba770b9106f24bd7
Zakarum <zakarum.vn@gmail.com>
2016-11-24 23:38:35 +0100

    Initial commit

---
+++ src/main/java/com/packt/kindergarten/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.packt.kindergarten.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by RM on 2016-11-20.
+ */
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String welcome(Model model) {
+        model.addAttribute("greeting", "Witaj w przedszkolu!");
+        return "welcome";
+    }
+}
---
+++ src/main/java/com/packt/kindergarten/domain/Employee.java
@@ -0,0 +1,95 @@
+//package com.packt.kindergarten.domain;
+//
+//import java.util.Date;
+//
+///**
+// * Created by RM on 2016-11-20.
+// */
+//public class Employee {
+//    private
+//    String name;
+//    String surname;
+//    String employeeID;
+//    String password;
+//    String address;
+//    Date employmentDate;
+//
+//    public Employee() {
+//
+//    }
+//
+//    public Employee(String name, String surname, String id, String password, String address) {
+//        this.name=name;
+//        this.surname=surname;
+//        this.employeeID=id;
+//        this.password=password;
+//        this.address;
+//        employmentDaye = new Date();
+//    }
+//    Boolean isEducator;
+//    ArrayList<Grade> teachClasses;
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getSurname() {
+//        return surname;
+//    }
+//
+//    public void setSurname(String surname) {
+//        this.surname = surname;
+//    }
+//
+//    public String getEmployeeID() {
+//        return employeeID;
+//    }
+//
+//    public void setEmployeeID(String employeeID) {
+//        this.employeeID = employeeID;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//
+//    public String getAddress() {
+//        return address;
+//    }
+//
+//    public void setAddress(String address) {
+//        this.address = address;
+//    }
+//
+//    public Date getEmploymentDate() {
+//        return employmentDate;
+//    }
+//
+//    public void setEmploymentDate(Date employmentDate) {
+//        this.employmentDate = employmentDate;
+//    }
+//
+//    public Boolean getEducator() {
+//        return isEducator;
+//    }
+//
+//    public void setEducator(Boolean educator) {
+//        isEducator = educator;
+//    }
+//
+//    public ArrayList<Grade> getTeachClasses() {
+//        return teachClasses;
+//    }
+//
+//    public void setTeachClasses(ArrayList<Grade> teachClasses) {
+//        this.teachClasses = teachClasses;
+//    }
+//}
