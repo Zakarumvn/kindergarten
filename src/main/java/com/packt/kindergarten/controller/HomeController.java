9a7b42b64195314f70e434852afd8ca49dc0cd08
ElKonrad <el.konradbuczkowski@gmail.com>
2016-11-25 18:30:17 +0100

    First-Commit-Konrad

--- src/main/java/com/packt/kindergarten/controller/HomeController.java
+++ src/main/java/com/packt/kindergarten/controller/HomeController.java
@@ -12,7 +12,7 @@ public class HomeController {
 
     @RequestMapping("/")
     public String welcome(Model model) {
-        model.addAttribute("greeting", "Witaj w przedszkolu! Pozdrowienia od Konrada, Siiiiiiuuuu");
+        model.addAttribute("greeting", "Witaj w przedszkolu! Pozdrowienia od Konrada, Siiiiiiuuuu, asdasdasdasd");
         return "welcome";
     }
 }
